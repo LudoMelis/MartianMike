d72eb7f950aff90a8944e2478bae39cf526ae597
Ludo Melis <ludomelis06@gmail.com>
2023-10-04 15:28:15 +0200

    Add basic movement to the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var speed = 125
+@export var jumpForce = 200
 @onready var animatedSprite = $AnimatedSprite2D
 
-func _process(delta):
-	# For testing purpose :
-	if Input.is_action_just_pressed("Right"):
-		animatedSprite.play("run")
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+	
+	var direction = Input.get_axis("Left", "Right")
+	
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = -jumpForce
+	
+	velocity.x = direction * speed
+	move_and_slide()
