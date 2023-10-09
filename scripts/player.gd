ac8a4a912bc7878f9c70218fddf77e27bcb85526
Ludo Melis <ludomelis06@gmail.com>
2023-10-09 14:52:38 +0200

    Add animations & fix infinite jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,9 +12,26 @@ func _physics_process(delta):
 			velocity.y = 500
 	
 	var direction = Input.get_axis("Left", "Right")
+	if direction != 0:
+		animatedSprite.flip_h = direction == -1
 	
-	if Input.is_action_just_pressed("Jump"):
+	if Input.is_action_just_pressed("Jump") && is_on_floor():
 		velocity.y = -jumpForce
 	
 	velocity.x = direction * speed
 	move_and_slide()
+	updateAnimation(direction)
+	
+	
+func updateAnimation(direction):
+	if is_on_floor():
+		if direction == 0:
+			animatedSprite.play("idle")
+		else:
+			animatedSprite.play("run")
+	else:
+		if velocity.y > 0:
+			animatedSprite.play("fall")
+		else:
+			animatedSprite.play("jump")
+	
