919bb9195f82ecf68a6e832b07ac5587e052d93f
Ludo Melis <ludomelis06@gmail.com>
2023-11-20 16:01:49 +0100

    Refactor & programmatically instance Timer & Hud

--- scripts/level.gd
+++ scripts/level.gd
@@ -3,12 +3,13 @@ extends Node2D
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var deathzone = $Deathzone
-@onready var hud = $UI/HUD
+@onready var ui = preload("res://scenes/ui.tscn")
 
 @export var nextLevel: PackedScene = null
 @export var levelTime = 5
 var player = null
 var timer = null
+var hud = null
 var timeLeft
 var win = false
 
@@ -23,14 +24,8 @@ func _ready():
 		trap.touchedPlayer.connect(_on_traps_touched_player)
 	exit.body_entered.connect(_on_exit_body_entered)
 	deathzone.body_entered.connect(_on_deathzone_body_entered)
-	timeLeft = levelTime
-	timer = Timer.new()
-	timer.name = "LevelTimer"
-	timer.wait_time = 1
-	timer.timeout.connect(_on_level_timer_out)
-	add_child(timer)
-	timer.start()
-	hud.setTimerLabel(timeLeft)
+	setTimer()
+	setUi()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,6 +34,22 @@ func _process(delta):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
+		
+func setTimer():
+	timeLeft = levelTime
+	timer = Timer.new()
+	timer.name = "LevelTimer"
+	timer.wait_time = 1
+	timer.timeout.connect(_on_level_timer_out)
+	add_child(timer)
+	timer.start()
+	
+
+func setUi():
+	var uiInstance = ui.instantiate()
+	add_child(uiInstance)
+	hud = uiInstance.get_node("HUD")
+	hud.setTimerLabel(timeLeft)
 
 func _on_level_timer_out():
 	if !win:
